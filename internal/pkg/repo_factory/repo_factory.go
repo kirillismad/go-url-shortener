39774456d3f83ae98618fa6f9b2053089625ba4c
Kirill Pech <pechkirill@gmail.com>
2024-06-15 16:41:01 +0300

    msg

--- internal/pkg/repo_factory/repo_factory.go
+++ internal/pkg/repo_factory/repo_factory.go
@@ -7,20 +7,22 @@ import (
 	"github.com/kirillismad/go-url-shortener/pkg/sqlc"
 )
 
+type NewRepoFn[R any] func(q *sqlc.Queries) R
+
 type RepoFactory[R any] struct {
-	db       *sql.DB
-	createFn func(q *sqlc.Queries) R
+	db        *sql.DB
+	newRepoFn NewRepoFn[R]
 }
 
-func NewRepoFactory[R any](db *sql.DB, createFn func(q *sqlc.Queries) R) *RepoFactory[R] {
+func NewRepoFactory[R any](db *sql.DB, newRepoFn NewRepoFn[R]) *RepoFactory[R] {
 	return &RepoFactory[R]{
-		db:       db,
-		createFn: createFn,
+		db:        db,
+		newRepoFn: newRepoFn,
 	}
 }
 
 func (r *RepoFactory[R]) GetRepo() R {
-	return r.createFn(sqlc.New(r.db))
+	return r.newRepoFn(sqlc.New(r.db))
 }
 
 func (r *RepoFactory[R]) InTransaction(ctx context.Context, txFn func(R) error) error {
@@ -30,7 +32,7 @@ func (r *RepoFactory[R]) InTransaction(ctx context.Context, txFn func(R) error) 
 	}
 	defer tx.Rollback()
 
-	if err := txFn(r.createFn(sqlc.New(tx))); err != nil {
+	if err := txFn(r.newRepoFn(sqlc.New(tx))); err != nil {
 		return err
 	}
 
